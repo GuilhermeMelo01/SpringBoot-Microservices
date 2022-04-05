44babd670b55bf507f997e726909887a22c53ebe
GuilhermeMelo01 <Meloguilherme1994@gmail.com>
2022-04-05 17:23:30 -0300

    Returning a List of objects

--- src/main/java/br/com/whiz/foo/service/PersonService.java
+++ src/main/java/br/com/whiz/foo/service/PersonService.java
@@ -3,6 +3,8 @@ package br.com.whiz.foo.service;
 import br.com.whiz.foo.model.Person;
 import org.springframework.stereotype.Service;
 
+import java.util.ArrayList;
+import java.util.List;
 import java.util.concurrent.atomic.AtomicLong;
 
 @Service  //Faz a injeção de dependecias dessa class onde for necessary
@@ -16,6 +18,25 @@ public class PersonService {
         person.setFirstName("Guilherme");
         person.setLastName("Melo");
         person.setAddress("Caucaia - CE");
+        person.setGender("Male");
+        return person;
+    }
+
+    public List<Person> findAll(){
+        List<Person> persons = new ArrayList<>();
+        for (int i = 0; i < 8; i++) {
+            Person person = mockPerson(i);
+            persons.add(person);
+        }
+        return persons;
+    }
+
+    private Person mockPerson(int i) {
+        Person person = new Person();
+        person.setId(counter.incrementAndGet());
+        person.setFirstName("first name" + i);
+        person.setLastName("last name" + i);
+        person.setAddress("some place in Brazil" + i);
         person.setGender("Male");
         return person;
     }
